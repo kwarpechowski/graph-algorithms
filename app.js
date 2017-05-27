2384e84ad25720fff5bf0299143b47f270c81c93
Kamil Warpechowski <k.warpechowski@hakama.pl>
2017-05-27 14:07:40 +0200

    step3

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-var t = [];
-var finalGroup = 0;
-var colors = [];
-var selected = [];
-var elements = [];
-var links = [];
-var n  = 0;
-var graphToAlg = [];
-
+let transponowaneWyniki = [];
+let colors = [];
+let selected = [];
+let elements = [];
+let links = [];
+let n  = 0;
+let graphToAlg = [];
+let isPrimary = [];
 
 const onClick = d => {
     if (selected.length < 2 && selected[0] !==  d.id) {
@@ -22,10 +21,8 @@ const onClick = d => {
 };
 
 const DFSprint = (i, visited, graph, results) => {
-    if(!visited[i]) {
-        visited[i] = true;
-        results.push(i);
-    }
+   visited[i] = true;
+   results.push(i);
 
     if (graph[i]) {
         graph[i].forEach(el => {
@@ -35,7 +32,6 @@ const DFSprint = (i, visited, graph, results) => {
         });
     }
 };
-
 
 const DFSstack = (i, visited, stack, graph) => {
     visited[i] = true;
@@ -48,9 +44,7 @@ const DFSstack = (i, visited, stack, graph) => {
     stack.push(i);
 };
 
-
 const printSCCs = graph => {
-
 
     let stack = [];
     let visited = graph.map(x => false);
@@ -61,52 +55,39 @@ const printSCCs = graph => {
         }
     }
 
-    let graph2 = [];
+    // transpozycja grafu
+    let graph2 = graph.map(x => []);
     for (let i = 0; i < graph.length; i++) {
-        graph[i].forEach(j => {
-            if (!Array.isArray(graph2[j])) {
-                graph2[j] = [];
-            }
-            graph2[j].push(i);
-        });
+        graph[i].forEach(j => graph2[j].push(i));
     }
 
-
-    visited = graph2.map(x => false);
-    const ile = stack.length;
+    visited = graph.map(x => false);
     let results =  [];
-    for(let i = 0; i< ile; i++) {
+    let tempResult = [];
+
+
+    while(stack.length) {
         const s = stack.pop();
 
         if(!visited[s]) {
-            DFSprint(i ,visited, graph2, results);
-            results.push(null);
+            DFSprint(s ,visited, graph2, tempResult);
+            results.push(tempResult);
+            tempResult = [];
         }
     }
 
-
-    for(let i = 0; i< ile; i++) {
-        if(results.indexOf(i) < 0) {
-            results.push(i);
-            results.push(null);
-        }
-    }
-
-    i = 0;
-
-    results.forEach(r => {
-        if(r !== null) {
-        t[r] = i;
-    } else {
-        i++;
-    }
-});
-
-    colors = d3.scaleLinear().domain([0,i])
+    colors = d3.scaleLinear().domain([0,results.length])
         .interpolate(d3.interpolateHcl)
         .range([d3.rgb("#007AFF"), d3.rgb('#FFF500')]);
 
+    transponowaneWyniki = [];
+
+    results.forEach((el, i) => {
+        el.forEach(j => transponowaneWyniki[j] = i)
+    });
+
     let macierz = [];
+    let i = results.length;
 
     for(let n = 0; n < i; n++) {
         macierz[n] = [];
@@ -115,21 +96,23 @@ const printSCCs = graph => {
         }
     }
 
-
     graph2.forEach((l,i) => {
         l.forEach(j => {
-            if (t[i] !== t[j]) {
-                macierz[t[i]][t[j]] = 1;
-                macierz[t[j]][t[i]] = 1;
+            if (transponowaneWyniki[i] !== transponowaneWyniki[j]) {
+                macierz[transponowaneWyniki[i]][transponowaneWyniki[j]] = 1;
+                macierz[transponowaneWyniki[j]][transponowaneWyniki[i]] = 1;
             }
          })
     });
 
-    finalGroup = macierz.reduce((p,c) => {
-        const ile = c.filter(x => x).length;
-        if(p > ile) return ile;
+    let m = macierz.map(m => m.filter(x => x).length).reduce((p, c) => {
+        if (c > p) {
+            p = c;
+        }
         return p;
     }, 0);
+
+    isPrimary = macierz.map(row => row.filter(x => x).length === m);
 };
 
 
@@ -139,10 +122,10 @@ const height = 500;
 
 const simulation = d3.forceSimulation()
     .velocityDecay(0.2)
-    .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(0.5))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force("collide", d3.forceCollide().radius(function(d) { return d.r + 0.5; }).iterations(5))
+    .force('link', d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(0.1))
+    .force('charge', d3.forceManyBody())
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('collide', d3.forceCollide().radius(function(d) { return d.r + 0.5; }).iterations(5))
 
 
 svg.append('defs').append('marker')
@@ -159,14 +142,12 @@ svg.append('defs').append('marker')
     .attr('fill', '#000')
     .style('stroke','none');
 
-
-
 const ticked = () => {
-    link.attr("x1", d => d.source.x)
-        .attr("y1", d => d.source.y)
-        .attr("x2", d => d.target.x)
-        .attr("y2", d => d.target.y );
-    node.attr("transform", d => 'translate(' +d.x + ',' + d.y +')')
+    link.attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y );
+    node.attr('transform', d => 'translate(' +d.x + ',' + d.y +')')
 };
 
 
@@ -176,17 +157,16 @@ const dragstarted = d => {
     d.fy = d.y;
 };
 
-function dragged(d) {
+const dragged = d => {
     d.fx = d3.event.x;
     d.fy = d3.event.y;
 }
 
-function dragended(d) {
+const dragended = d => {
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
-}
-
+};
 
 
 window.add = function() {
@@ -204,40 +184,41 @@ window.addRel = function(x, y) {
         target: y
     });
     graphToAlg[x].push(y);
-    draw();
+   draw();
 };
 
 
 const draw = () => {
-    printSCCs(graphToAlg);
-    svg.selectAll("g").remove().exit();
 
-    link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
+    printSCCs(graphToAlg);
+    svg.selectAll('g').remove().exit();
+
+    link = svg.append('g')
+        .attr('class', 'links')
+        .selectAll('line')
         .data(links)
-        .enter().append("line")
-        .attr("stroke", '#000')
-        .attr("stroke-width", 2)
+        .enter().append('line')
+        .attr('stroke', '#000')
+        .attr('stroke-width', 2)
         .attr('marker-end','url(#arrowhead)');
 
-    node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("g")
+    node = svg.append('g')
+        .attr("class", 'nodes')
+        .selectAll('g')
         .data(elements)
-        .enter().append("g")
+        .enter().append('g')
         .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
+            .on('start', dragstarted)
+            .on('drag', dragged)
+            .on('end', dragended));
 
 
-    node.append("circle").attr('r', 20)
-        .attr("fill", d => colors(t[d.id]))
-    .attr("stroke-width", 5)
-        .attr("stroke", d => t[d.id] === finalGroup ? '#000' : 'none' )
-    .on("click", onClick);
-    node.append("text")
+    node.append('circle').attr('r', 20)
+        .attr('fill', d => colors(transponowaneWyniki[d.id]))
+    .attr('stroke-width', 5)
+        .attr('stroke', d => isPrimary[transponowaneWyniki[d.id]] ? '#000' : 'none' )
+    .on('click', onClick);
+    node.append('text')
         .attr('dy', 5)
         .attr('dx', 0)
         .attr('fill', '#000')
@@ -252,3 +233,24 @@ const draw = () => {
 
 
 svg.on('dblclick', add);
+
+
+document.querySelector('#loadData').addEventListener('click', () => {
+    add(); add(); add(); add();
+    add(); add(); add(); add();
+
+    addRel(0, 1);
+    addRel(1, 2);
+    addRel(1, 4);
+    addRel(1, 6);
+    addRel(2, 0);
+    addRel(2, 3);
+    addRel(3, 4);
+    addRel(4, 5);
+    addRel(5, 3);
+    addRel(6, 7);
+});
+
+
+
+
